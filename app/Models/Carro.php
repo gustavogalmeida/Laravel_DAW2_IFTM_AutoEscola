f7cd2130dfba3bc58dc0365cd0c1cb9160797e30
Gustavo <gustavogalvesalmeida@gmail.com>
2022-03-17 20:43:31 -0300

    estilizando com Bootstrap

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Carro extends Model
+class Carro extends Model 
+
+/*
+MODEL: Onde fica os vinculos com as tables do banco do .env 
+como criar model:
+php artisan make:model NomeModel -m
+*/
 {
     use HasFactory;
     protected $table= "carros";
